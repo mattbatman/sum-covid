c3affc9b81df1ac9f9ccc8a2cec88adc09885499
Matt Batman <batman.matt@gmail.com>
2021-08-27 16:54:48 -0400

    Add tooltip to line chart

--- src/line-chart.js
+++ src/line-chart.js
@@ -19,6 +19,9 @@ const LineChart = () => {
   let subtitle = '';
   let tooltip;
   let tipBody;
+  let dot;
+  let path;
+  let displayDate = d3.timeFormat('%b %e, %g');
 
   function init({ sel, xTitle, chartTitle, subtitle, newMargin }) {
     selector = sel;
@@ -103,7 +106,8 @@ const LineChart = () => {
 
     tipBody = tooltip.append('tbody');
 
-    tipBody.append('tr').append('td').attr('class', 'stat');
+    tipBody.append('tr').append('td').attr('class', 'first-row');
+    tipBody.append('tr').append('td').attr('class', 'second-row')
   }
 
   function update({ newData, xKey, yKey }) {
@@ -129,20 +133,120 @@ const LineChart = () => {
       .x((d) => xScale(d[xk]))
       .y((d) => yScale(d[yk]));
 
-    svg
-      .append('path')
-      .datum(data)
+    path = svg
+      .append('g')
       .attr('fill', 'none')
       .attr('class', 'yellow-line')
       .attr('stroke', 'yellow')
       .attr('stroke-width', 1.5)
       .attr('stroke-linejoin', 'round')
       .attr('stroke-linecap', 'round')
+      .selectAll('path')
+      .data([data])
+      .join('path')
+      .style('mix-blend-mode', 'multiply')
       .attr('d', line);
 
     // call the axes
     svg.select('.x.axis').call(xAxis);
     svg.select('.y.axis').call(yAxis);
+
+    // hover effects
+    svg.on('mousemove', moved).on('mouseenter', entered).on('mouseleave', left);
+
+    dot = svg.append('g').attr('display', 'none');
+
+    dot.append('circle').attr('r', 2.5);
+
+    dot
+      .append('text')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', 10)
+      .attr('text-anchor', 'middle')
+      .attr('y', -8);
+  }
+
+  function moved(event) {
+    event.preventDefault();
+    const pointer = d3.pointer(event, this);
+    const xm = xScale.invert(pointer[0]);
+    const ym = yScale.invert(pointer[1]);
+    const i = d3.bisectCenter(
+      data.map((d) => d[xk]),
+      xm
+    );
+    const s = d3.least(data, (d) => Math.abs(d[yk] - ym));
+    path
+      .classed('gradient-path', (d) => (d === s ? false : true))
+      .filter((d) => d === s)
+      .raise();
+    dot.attr(
+      'transform',
+      `translate(${xScale(data[i][xk])},${yScale(data[i][yk])})`
+    );
+    
+    updateTooltip(event, [xk], displayDate(data[i][xk]), [yk], data[i][yk])
+  }
+
+  function entered() {
+    path.style('mix-blend-mode', null).classed('gradient-path', false);
+    dot.attr('display', null);
+  }
+
+  function left() {
+    path.style('mix-blend-mode', 'multiply').classed('gradient-path', false);
+    dot.attr('display', 'none');
+    d3.select(`${selector} .d3-tooltip`).classed('hidden', true);
+  }
+
+  function updateTooltip(event, firstRowLabel = '', firstRowKey = '', secondRowLabel = '', secondRowKey = '') {
+    d3.select(`${selector} .d3-tooltip .first-row`).text(
+      `${firstRowLabel}: ${firstRowKey.toLocaleString()}`
+    );
+d3.select(`${selector} .d3-tooltip .second-row`).text(
+      `${secondRowLabel}: ${secondRowKey.toLocaleString()}`
+    );
+    d3.select(`${selector} .d3-tooltip`)
+      .classed('hidden', false)
+      .style('left', () => {
+        if (
+          parseInt(
+            `${
+              event.pageX +
+              0.5 *
+                parseInt(d3.select(`${selector} .d3-tooltip`).style('width'))
+            }`
+          ) > parseInt(`${parseInt(d3.select('body').style('width'))}`)
+        ) {
+          return `${
+            event.pageX -
+            parseInt(d3.select(`${selector} .d3-tooltip`).style('width'))
+          }px`;
+        }
+        if (
+          parseInt(
+            `${
+              event.pageX -
+              0.5 *
+                parseInt(d3.select(`${selector} .d3-tooltip`).style('width'))
+            }`
+          ) < 0
+        ) {
+          return `${event.pageX}px`;
+        }
+        return `${
+          event.pageX -
+          0.5 * parseInt(d3.select(`${selector} .d3-tooltip`).style('width'))
+        }px`;
+      })
+      .style(
+        'top',
+        `${
+          event.pageY -
+          parseInt(d3.select(`${selector} .d3-tooltip`).style('height')) -
+          7
+        }px`
+      );
   }
 
   function resize(event) {
@@ -168,7 +272,7 @@ const LineChart = () => {
       .x((d) => xScale(d[xk]))
       .y((d) => yScale(d[yk]));
 
-    svg.select('path.yellow-line').attr('d', line);
+    path.attr('d', line);
   }
 
   return { init, update };
