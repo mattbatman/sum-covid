656daeff9c462c54da4d00ff5529970ed3f10390
Matt Batman <batman.matt@gmail.com>
2021-09-30 12:40:14 -0400

    Pretty

--- src/vaers-event-chart.js
+++ src/vaers-event-chart.js
@@ -5,20 +5,20 @@ import rawData from './data/all-vaccine-events.json';
 function vaersEvent(eventCategory, selector, leftMargin = 45) {
   const chart = barChart();
   const filterAndSort = pipe(
-          (data) => filter((d) => d.event_category === eventCategory, data),
-          (data) => filter((d) => d.year_reported !== 'Unknown Date', data),
-          (data) =>
-            map(
-              (d) => ({
-                ...d,
-                Year: d.year_reported,
-                [eventCategory]: Number(d.events_reported)
-              }),
-              data
-            ),
-          (data) => sort((a, b) => a.Year - b.Year, data),
-          (data) => filter((d) => d.Year >= 1990, data)
-        );
+    (data) => filter((d) => d.event_category === eventCategory, data),
+    (data) => filter((d) => d.year_reported !== 'Unknown Date', data),
+    (data) =>
+      map(
+        (d) => ({
+          ...d,
+          Year: d.year_reported,
+          [eventCategory]: Number(d.events_reported)
+        }),
+        data
+      ),
+    (data) => sort((a, b) => a.Year - b.Year, data),
+    (data) => filter((d) => d.Year >= 1990, data)
+  );
 
   chart.init({
     sel: selector,
