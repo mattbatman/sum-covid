523d1467fd6bb34317fdc0ef08e11a9292c60697
Matt Batman <batman.matt@gmail.com>
2021-10-23 13:32:57 -0400

    Update VAERS data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import smoothScrollArrow from './scroll-arrow';
 import daily from './daily-deaths-cases';
 import weekly from './weekly-flu';
 import causes from './select-causes-of-death';
-import vaersDeathsByYear from './vaers-deaths-by-year';
 import vaersEvent from './vaers-event-chart';
 import countData from './data/combined-by-age.json';
 
@@ -15,7 +14,7 @@ function main() {
   daily();
   weekly();
   causes();
-  vaersDeathsByYear();
+  vaersEvent('Death', '#vaers-deaths-by-year', 55)
   vaersEvent('Permanent Disability', '#vaers-disability-by-year', 55);
   vaersEvent('Hospitalized', '#vaers-hospitalizations-by-year', 55);
 }
